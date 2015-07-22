fe38541efca750975241e584c9f809cf7a08bc4e
sv <svashis@outlook.com>
2015-07-22 10:09:51 -0700

    first commit

---
+++ Shaurya/SV7.py
@@ -0,0 +1,15 @@
+
+import turtle
+wd = turtle.Screen()
+Shaurya = turtle.Turtle()
+wd.bgcolor("black")
+Shaurya.color("red")
+Shaurya.speed(0)
+for i in range(0,96):
+	Shaurya.fd(130)
+	Shaurya.lt(130)
+	Shaurya.lt(120) 
+Shaurya.reset()
+wd.exitonclick()
+
+
